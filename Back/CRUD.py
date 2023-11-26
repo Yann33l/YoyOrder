06024cee68c052c1f7cba076b89c80fffc925fd9
Yann33l <111899082+Yann33l@users.noreply.github.com>
2023-11-26 22:09:53 +0100

    Back : Creation schemas et models

--- Back/CRUD.py
+++ Back/CRUD.py
@@ -47,3 +47,172 @@ def edit_autorisation_status(db: Session, email: str, autorisation: bool):
         db.refresh(db_user)
     return db_user
 
+
+# Articles
+def get_articles(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.articles).offset(skip).limit(limit).all()
+
+
+def create_article(db: Session, article: schemas.Articles):
+    db_article = models.articles(
+        ID=article.ID,
+        libelle=article.libelle,
+        ref=article.ref,
+        fournisseur_id=article.fournisseur_id,
+        conditionnement=article.conditionnement,
+        dateDebutValidite=article.dateDebutValidite,
+        dateFinValidite=article.dateFinValidite,
+    )
+    db.add(db_article)
+    db.commit()
+    return db_article
+
+# Fournisseurs
+
+
+def get_fournisseurs(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.fournisseurs).offset(skip).limit(limit).all()
+
+
+def create_fournisseur(db: Session, fournisseur: schemas.Fournisseurs):
+    db_fournisseur = models.fournisseurs(
+        ID=fournisseur.ID,
+        libelle=fournisseur.libelle,
+        telephone=fournisseur.telephone,
+        email=fournisseur.email,
+        siteWeb=fournisseur.siteWeb,
+        getCertificatAnalyse=fournisseur.getCertificatAnalyse,
+    )
+    db.add(db_fournisseur)
+    db.commit()
+    return db_fournisseur
+
+
+def edit_fournisseur(db: Session, fournisseur: schemas.Fournisseurs):
+    db_fournisseur = db.query(models.fournisseurs).filter(
+        models.fournisseurs.ID == fournisseur.ID).scalar()
+    if db_fournisseur:
+        db_fournisseur.libelle = fournisseur.libelle
+        db_fournisseur.telephone = fournisseur.telephone
+        db_fournisseur.email = fournisseur.email
+        db_fournisseur.siteWeb = fournisseur.siteWeb
+        db_fournisseur.getCertificatAnalyse = fournisseur.getCertificatAnalyse
+        db.commit()
+        db.refresh(db_fournisseur)
+    return db_fournisseur
+
+# Secteurs
+
+
+def get_secteurs(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.secteurs).offset(skip).limit(limit).all()
+
+
+def create_secteur(db: Session, secteur: schemas.Secteurs):
+    db_secteur = models.secteurs(
+        ID=secteur.ID,
+        libelle=secteur.libelle,
+    )
+    db.add(db_secteur)
+    db.commit()
+    return db_secteur
+
+
+def edit_secteur(db: Session, secteur: schemas.Secteurs):
+    db_secteur = db.query(models.secteurs).filter(
+        models.secteurs.ID == secteur.ID).scalar()
+    if db_secteur:
+        db_secteur.libelle = secteur.libelle
+        db.commit()
+        db.refresh(db_secteur)
+    return db_secteur
+
+# Stocks
+
+
+def get_stocks(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.stocks).offset(skip).limit(limit).all()
+
+
+def create_stock(db: Session, stock: schemas.Stocks):
+    db_stock = models.stocks(
+        ID=stock.ID,
+        article_id=stock.article_id,
+        quantiteInitiale=stock.quantiteInitiale,
+        quantiteRestante=stock.quantiteRestante,
+        lot=stock.lot,
+        datePeremption=stock.datePeremption,
+        COA=stock.COA,
+    )
+    db.add(db_stock)
+    db.commit()
+    return db_stock
+
+
+def edit_stock(db: Session, stock: schemas.Stocks):
+    db_stock = db.query(models.stocks).filter(
+        models.stocks.ID == stock.ID).scalar()
+    if db_stock:
+        db_stock.article_id = stock.article_id
+        db_stock.quantiteInitiale = stock.quantiteInitiale
+        db_stock.quantiteRestante = stock.quantiteRestante
+        db_stock.lot = stock.lot
+        db_stock.datePeremption = stock.datePeremption
+        db_stock.COA = stock.COA
+        db.commit()
+        db.refresh(db_stock)
+    return db_stock
+
+# Gestion des couts
+
+
+def get_gestiondescouts(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.gestiondescouts).offset(skip).limit(limit).all()
+
+
+def create_gestiondescout(db: Session, gestiondescout: schemas.GestionDesCouts):
+    db_gestiondescout = models.gestiondescouts(
+        ID=gestiondescout.ID,
+        article_id=gestiondescout.article_id,
+        prixUnitaire=gestiondescout.prixUnitaire,
+        dateDebutValidite=gestiondescout.dateDebutValidite,
+        dateFinValidite=gestiondescout.dateFinValidite,
+    )
+    db.add(db_gestiondescout)
+    db.commit()
+    return db_gestiondescout
+
+
+# Commandes
+def get_commandes(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.commandes).offset(skip).limit(limit).all()
+
+
+def create_commande(db: Session, commande: schemas.Commandes):
+    db_commande = models.commandes(
+        ID=commande.ID,
+        article_id=commande.article_id,
+        quantite=commande.quantite,
+        secteur_id=commande.secteur_id,
+        dateCommande=commande.dateCommande,
+        dateReception=commande.dateReception,
+        dateAchat=commande.dateAchat,
+    )
+    db.add(db_commande)
+    db.commit()
+    return db_commande
+
+
+def edit_commande(db: Session, commande: schemas.Commandes):
+    db_commande = db.query(models.commandes).filter(
+        models.commandes.ID == commande.ID).scalar()
+    if db_commande:
+        db_commande.article_id = commande.article_id
+        db_commande.quantite = commande.quantite
+        db_commande.secteur_id = commande.secteur_id
+        db_commande.dateCommande = commande.dateCommande
+        db_commande.dateReception = commande.dateReception
+        db_commande.dateAchat = commande.dateAchat
+        db.commit()
+        db.refresh(db_commande)
+    return db_commande
--- Back/schemas.py
+++ Back/schemas.py
@@ -3,7 +3,7 @@ from datetime import date
 from pydantic import BaseModel
 
 
-# Token
+# --------------------------------------------------------------------------------
 class Token(BaseModel):
     access_token: str
     token_type: str
@@ -14,7 +14,9 @@ class TokenData(BaseModel):
     Admin: bool
     Autorisation: bool
 
-# table users
+# --------------------------------------------------------------------------------
+
+
 class UserForm(BaseModel):
     Email: str
     Password: str
@@ -38,8 +40,106 @@ class UserBase(BaseModel):
     class Config:
         orm_mode = True
 
+
 class UserCreate(UserBase):
     Password: bytes
 
+# --------------------------------------------------------------------------------
 
-# table 
+
+class Articles(BaseModel):
+    ID: int
+    libelle: str
+    ref: str
+    fournisseur_id: int
+    conditionnement: str
+    dateDebutValidite: date
+    dateFinValidite: date
+
+
+class Fournisseurs(BaseModel):
+    ID: int
+    libelle: str
+    telephone: str
+    email: str
+    siteWeb: str
+    getCertificatAnalyse: str
+
+
+class Commandes(BaseModel):
+    ID: int
+    article_id: int
+    quantite: int
+    secteur_id: int
+    dateCommande: date
+    dateReception: date
+    dateAchat: date
+
+
+class Secteurs(BaseModel):
+    ID: int
+    libelle: str
+
+
+class Stocks(BaseModel):
+    ID: int
+    article_id: int
+    quantite: int
+    secteur_id: int
+    dateAchat: date
+    datePeremption: date
+    dateReception: date
+
+
+class GestionDesCouts(BaseModel):
+    ID: int
+    article_id: int
+    prixUnitaire: date
+    dateDebutValidite: date
+    dateFinValidite: date
+
+
+class LieuxDeStockage(BaseModel):
+    ID: int
+    libelle: str
+    temperature: int
+
+
+class r_articles_lieux(BaseModel):
+    ID: int
+    article_id: int
+    lieuxDeStockage_id: int
+
+
+class r_articles_secteurs(BaseModel):
+    ID: int
+    article_id: int
+    secteur_id: int
+
+
+class r_user_stock(BaseModel):
+    ID: int
+    user_id: int
+    stock_id: int
+    quantiteUpdtae: int
+    dateUpdate: date
+
+
+class r_user_commande(BaseModel):
+    ID: int
+    user_id: int
+    commande_id: int
+    dateUpdate: date
+
+
+class r_user_secteur(BaseModel):
+    ID: int
+    user_id: int
+    secteur_id: int
+
+
+class usersdates(BaseModel):
+    ID: int
+    user_id: int
+    date: date
+    statut: str
